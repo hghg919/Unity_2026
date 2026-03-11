9a3ebeecc41a8cb497b33094f1e1525c82c42dd5
hghg919 <tgeq1023@naver.com>
2026-03-11 16:43:32 +0900

    (2026.03.11) 2주차-2
    본수업

---
+++ Unity/Assets/HelloCode.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloCode : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Debug.Log("hello world!");
+
+        //string Name = "라라";
+        //char bloodType = 'A';
+        //int age = 17;
+        //float height = 168.3f;
+        //bool isFemale = true;
+        //var country = "영국";
+
+        //Debug.Log("캐릭터 이름 : " + Name);
+        //Debug.Log("혈액형 : " + bloodType);
+        //Debug.Log("나이 : " + age);
+        //Debug.Log("키 : " + height);
+        //Debug.Log("여성인가? : " + isFemale);
+        //Debug.Log("출신 국가 : " + country);
+
+        //int age = 11;
+
+        //if (age > 7 && age < 18)
+        //{
+        //    Debug.Log("의무 교육을 받고 있습니다");
+        //}
+        //if (age < 13 || age > 70)
+        //{
+        //    Debug.Log("일을 할 수 없는 나이입니다.");
+        //}
+
+        //    float distance = GetDistance(2, 2, 5, 6);
+        //    Debug.Log("(2,2)에서 (5,6)까지의 거리 : " + distance);
+        //}
+
+        //float GetDistance(float x1, float y1, float x2, float y2)
+        //{
+        //    float width = x2 - x1;
+        //    float height = y2 - y1;
+
+        //    float distance = width * width + height * height;
+        //    distance = Mathf.Sqrt(distance);
+
+        //    return distance;
+        //for (int i = 0;i < 10;i++)
+        //{
+        //    Debug.Log(i + " 번째 순번입니다.");
+        //}
+
+        //int i = 0;
+
+        //while (i < 10)
+        //{
+        //    Debug.Log(i + " 번째 루프입니다.");
+        //    i++;
+        //}
+
+        bool isDead = false;
+        int hp = 100;
+
+        while (!isDead)
+        {
+            Debug.Log("현재 체력 : " + hp);
+
+            hp = hp - 33;
+
+            if(hp <= 0 )
+            {
+                isDead = true;
+                Debug.Log("플레이어는 죽었습니다.");
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
