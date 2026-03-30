5f9abf39691706af3eff8192bc5cc7820b2445e2
hghg919 <tgeq1023@naver.com>
2026-03-30 17:14:44 +0900

    백업

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody.AddForce(0f,0f,speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+    }
+}
