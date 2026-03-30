51779de676f1cd4004b3d524e49e871e46f72d7f
hghg919 <tgeq1023@naver.com>
2026-03-30 20:49:48 +0900

    과제
    과제제출

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.linearVelocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            //Destroy(gameObject);
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -7,27 +7,33 @@ public class PlayerController : MonoBehaviour
     public float speed = 8f;
     void Start()
     {
-        
+        playerRigidbody =GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(xInput, 0f, zInput).normalized;
+
+        Vector3 newVelocity = new Vector3(xInput, 0f, zInput).normalized;
+        newVelocity *= speed;
+        playerRigidbody.linearVelocity = newVelocity;
+
+        if (movement != Vector3.zero)
         {
-            playerRigidbody.AddForce(0f,0f,speed);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(movement),
+                10f * Time.deltaTime
+            );
         }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
